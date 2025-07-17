1fe06ea26e006076262404f7ea65dd62b899d118
nima <nimagharibnavaz58@gmail.com>
2025-07-17 14:54:19 +0330

    matlab script.m

--- Untitled.m
+++ Untitled.m
@@ -1,15 +1,15 @@
-delete(instrfind);
+delete(instrfind); %delete all of the port that available
 
-s =serial('COM2');
-fopen(s);
+s =serial('COM2'); %chose the port
+fopen(s); %open the port
 i=1;
 data=12.1;
 
-while(i<200)
-    a = fscanf(s);
-    data(i)= str2num(a(4:end-1));
-    plot(data);
-    pause(0.01);
+while(i<200) %repeat the process 200 time 
+    a = fscanf(s); %get the temprecher 
+    data(i)= str2num(a(4:end-1)); %delete the charecter 
+    plot(data); 
+    pause(0.01); %delay 
     i = i+1;
 end
-fclose(s);
+fclose(s); %close the port
